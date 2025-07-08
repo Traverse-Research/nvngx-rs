b71e6bd3aa8877812a53d45666dd5492e82881f8
Marijn Suijten <marijn@traverseresearch.nl>
2025-07-08 14:51:40 +0200

    nvngx-sys: Replace open-coded `Vk*` definitions with `ash::vk::*`

--- crates/nvngx-sys/src/lib.rs
+++ crates/nvngx-sys/src/lib.rs
@@ -5,6 +5,14 @@
 #![allow(non_snake_case)]
 #![allow(clippy::all)]
 
+use ash::vk::{
+    Buffer as VkBuffer, CommandBuffer as VkCommandBuffer, Device as VkDevice,
+    ExtensionProperties as VkExtensionProperties, Format as VkFormat, Image as VkImage,
+    ImageSubresourceRange as VkImageSubresourceRange, ImageView as VkImageView,
+    Instance as VkInstance, PFN_vkGetDeviceProcAddr, PFN_vkGetInstanceProcAddr,
+    PhysicalDevice as VkPhysicalDevice,
+};
+
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 pub mod error;
--- crates/nvngx/src/vk/feature.rs
+++ crates/nvngx/src/vk/feature.rs
@@ -487,8 +487,8 @@ impl Feature {
         let mut handle = FeatureHandle::new();
         Result::from(unsafe {
             nvngx_sys::NVSDK_NGX_VULKAN_CreateFeature1(
-                device.as_pointer_mut(),
-                command_buffer.as_pointer_mut(),
+                device,
+                command_buffer,
                 feature_type,
                 parameters.0,
                 &mut handle.0 as *mut _,
@@ -520,8 +520,8 @@ impl Feature {
         unsafe {
             let mut handle = FeatureHandle::new();
             Result::from(nvngx_sys::HELPERS_NGX_VULKAN_CREATE_DLSS_EXT1(
-                device.as_pointer_mut(),
-                command_buffer.as_pointer_mut(),
+                device,
+                command_buffer,
                 1,
                 1,
                 &mut handle.0 as *mut _,
@@ -572,8 +572,8 @@ impl Feature {
         unsafe {
             let mut handle = FeatureHandle::new();
             Result::from(nvngx_sys::HELPERS_NGX_VULKAN_CREATE_DLSSD_EXT1(
-                device.as_pointer_mut(),
-                command_buffer.as_pointer_mut(),
+                device,
+                command_buffer,
                 1,
                 1,
                 &mut handle.0 as *mut _,
@@ -660,7 +660,7 @@ impl Feature {
     pub fn evaluate(&self, command_buffer: vk::CommandBuffer) -> Result {
         unsafe {
             nvngx_sys::NVSDK_NGX_VULKAN_EvaluateFeature_C(
-                command_buffer.as_pointer_mut(),
+                command_buffer,
                 self.handle.0,
                 self.parameters.0,
                 Some(feature_progress_callback),
